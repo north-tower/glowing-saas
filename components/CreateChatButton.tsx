8b4f819a4b9d39f1539262c855790b23fbe15aba
north-tower <muneshmyke@gmail.com>
2024-05-05 01:12:26 +0300

    mm

---
+++ components/CreateChatButton.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from 'react'
+import { MessageSquarePlusIcon } from 'lucide-react'
+import { Button } from './ui/button'
+import { useRouter } from 'next/navigation'
+
+
+function CreateChatButton() {
+    const router = useRouter();
+    
+    const createNewChat = async () => {
+        router.push(`/chat/abc`);
+    }
+  return (
+    <Button variant={"ghost"}>
+        <Button onClick={createNewChat} variant={"ghost"}>
+        <MessageSquarePlusIcon />
+        </Button>
+    </Button>
+  )
+}
+
+export default CreateChatButton
