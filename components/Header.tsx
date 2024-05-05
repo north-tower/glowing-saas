d28c6106d47eedd8853693aa79a0b848e996ddc4
north-tower <muneshmyke@gmail.com>
2024-05-05 16:09:25 +0300

    df

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { authOptions } from "@/auth"
 import Link from "next/link"
 import { MessagesSquareIcon } from "lucide-react"
 import CreateChatButton from "./CreateChatButton"
+import UpgradeBanner from "./UpgradeBanner"
 
 async function Header() {
   const session = await getServerSession(authOptions);
@@ -37,7 +38,7 @@ async function Header() {
         </div>
       </nav>
 
-
+        <UpgradeBanner />
     </header>
   )
 }
