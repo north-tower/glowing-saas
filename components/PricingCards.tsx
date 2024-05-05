43b01661ee2768695bc1ab670e16dc7891947d8d
north-tower <muneshmyke@gmail.com>
2024-05-05 09:29:05 +0300

    hj

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -1,3 +1,5 @@
+import { CheckIcon } from "lucide-react";
+import Link from "next/link";
 import { features } from "process"
 
 const tiers = [
@@ -35,7 +37,57 @@ const tiers = [
 
 function PricingCards({ redirect } : { redirect: boolean } ) {
   return (
-    <div>PricingCards</div>
+    <div className="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
+      {tiers.map((tier) => (
+        <div 
+        key={tier.id + tier.name} 
+        className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
+        >
+          <div>
+            <h3 className="text-base font-semibold leading-7 text-indigo-600">
+              {tier.name}
+            </h3>
+            <div className="mt-4 flex items-baseline gap-x-2">
+              {tier.priceMonthly ? (
+                <>
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">
+                    {tier.priceMonthly}
+                  </span>
+                  <span className="text-base font-semibold leading-7 text-gray-600">
+                    /month
+                  </span>
+                </>
+              ) : (
+                <span className="text-5xl font-bold tracking-tight text-gray-600">
+                  Free
+                </span>
+              )}
+            </div>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              {tier.description}
+            </p>
+            <ul role="list" className="mt-10 space-y-4 text-sm leading-4 text-gray-600">
+              {tier.features.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <CheckIcon className="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+
+                  {feature}
+                </li>
+              ))}
+
+            </ul>
+          
+           </div>
+           {redirect ? (
+            <Link href="/register">
+              Get Started Today
+            </Link>
+           ) : (
+            tier.id && <CheckOutButton />
+           )}
+           </div>
+      ))}
+    </div>
   )
 }
 
