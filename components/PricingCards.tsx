45aa1e143c86168edb1cdb237a41eb07ddfb19e7
north-tower <muneshmyke@gmail.com>
2024-05-05 08:04:31 +0300

    Pricing

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -1,6 +1,39 @@
+import { features } from "process"
 
+const tiers = [
+  {
+    name: "Starter",
+    id: null,
+    href: "#",
+    priceMonthly: null,
+    description: "Get chatting right away with anyone, anywhere",
+    features: [
+      "20 message Chat Linit in chats",
+      "2 Participant linit in Chat",
+      "3 Chat Rooms, limit",
+      "Supports 2 languages",
+      "48-hour support response time",
+    ],
+  },
+  {
+    name: "Pro",
+    id: "si_OnlcsLNQYbMVzV",
+    href: "#",
+    priceMonthly: "$5.66",
+    description: "Unlock the Full Potential with Pro!",
+    features: [
+      "Unlimited Messages in Chats",
+      "Unlimited Participants in Chats",
+      "Unlimited Chat Rooms",
+      "Supports up to 10 languages",
+      "Multimedia support in chats(coming soon)",
+      "1-hour, dedicated support response time",
+      "Early access to New Features",
+    ],
+  },
+];
 
-function PricingCards() {
+function PricingCards({ redirect } : { redirect: boolean } ) {
   return (
     <div>PricingCards</div>
   )
