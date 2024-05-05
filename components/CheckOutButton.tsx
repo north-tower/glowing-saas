ca623ef05b5427f20bdde7f8b4c0424fa6553419
north-tower <muneshmyke@gmail.com>
2024-05-05 11:08:04 +0300

    hj

--- components/CheckOutButton.tsx
+++ components/CheckOutButton.tsx
@@ -1,8 +1,15 @@
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 function CheckOutButton() {
+  const { data: session} = useSession()
+
+  const createCheckOutSession = async () => {
+    if(!session) return
+
+  }
   return (
-    <button className='mt-8 block rounded-md bg-indigo-600 
+    <button onClick={() => createCheckOutSession()} className='mt-8 block rounded-md bg-indigo-600 
     px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500
     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
     focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 
