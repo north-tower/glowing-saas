5da49f394d8f65a9e9be4af23a423354da8f53f5
north-tower <muneshmyke@gmail.com>
2024-05-05 10:18:46 +0300

    qw

--- components/CheckOutButton.tsx
+++ components/CheckOutButton.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function CheckOutButton() {
   return (
-    <div>CheckOutButton</div>
+    <div className='mt-8 block rounded-md bg-indigo-600 
+    px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500
+    focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+    focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 
+    disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default
+    '> CheckOutButton</div>
   )
 }
 
