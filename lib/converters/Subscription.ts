b8b22301706500963438271d5ae332dfb8195e16
north-tower <muneshmyke@gmail.com>
2024-05-05 15:41:00 +0300

    qw

---
+++ lib/converters/Subscription.ts
@@ -0,0 +1,33 @@
+import { db } from "@/firebase"
+
+import { Subscription } from "@/types/Subscription"
+import {
+    DocumentData, FirestoreDataConverter,
+    QueryDocumentSnapshot,
+    SnapshotOptions, collection
+} from "firebase/firestore"
+
+const subscriptionConverter: FirestoreDataConverter<Subscription> = {
+    toFirestore: function (subscription: Subscription): DocumentData {
+        return {
+            ...subscription
+        }
+    }, 
+    fromFirestore: function ( snapshot: QueryDocumentSnapshot, options: SnapshotOptions ) : Subscription {
+        const data = snapshot.data(options);
+
+        const sub: Subscription = {
+            id: snapshot.id,
+            role: data.role,
+            price: data.price,
+            amount: data.amount,
+
+        }
+
+        return sub;
+    }
+}
+export const subscriptionRef = (userId: string) => 
+    collection(db, "customers", userId, "subscriptions").withConverter(
+        subscriptionConverter
+    );
