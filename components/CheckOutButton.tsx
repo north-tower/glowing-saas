08330d0f542c27991b44a91959b369e8c6bd67ff
north-tower <muneshmyke@gmail.com>
2024-06-01 22:48:51 +0300

    bn

--- components/CheckOutButton.tsx
+++ components/CheckOutButton.tsx
@@ -5,6 +5,7 @@ import { addDoc, collection, onSnapshot } from 'firebase/firestore';
 import { useSession } from 'next-auth/react'
 import Link from 'next/link';
 import React, { useState } from 'react'
+import LoadingSpinner from './LoadingSpinner';
 
 function CheckOutButton({ sub } : { sub: string }) {
   const { data: session} = useSession();
@@ -31,7 +32,7 @@ function CheckOutButton({ sub } : { sub: string }) {
     focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
     focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 
     disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default
-    '>{loading ? "Loading.." : "Sign Up"}</Link>
+    '>{loading ? <LoadingSpinner /> : "Sign Up"}</Link>
   )
 }
 
--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -7,8 +7,12 @@ import UserAvatar from "./UserAvatar";
 import { Session } from "inspector";
 import { Button } from "./ui/button";
 import { signIn, signOut } from "next-auth/react";
+import LoadingSpinner from "./LoadingSpinner";
+import { useSubscriptionStore } from "@/store/store";
+import { StarIcon } from "lucide-react";
 
 function UserButton({ session }: { session: Session | null}) {
+    const subscription = useSubscriptionStore((state) => state.subscription)
     if (!session)
         return (
             <Button variant={"outline"} onClick={() => signIn()}>
@@ -25,7 +29,28 @@ function UserButton({ session }: { session: Session | null}) {
             <DropdownMenuContent>
                 <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
+                {subscription === undefined && (
+                    <DropdownMenuItem>
+                        <LoadingSpinner />
+                    </DropdownMenuItem>
                 
+                )}
+
+                {subscription?.role === "pro" && (
+                    <>
+                    <DropdownMenuLabel className="text-xs flex items-center justify-center space-x-1 
+                    text-[#f935c1]
+                    animate-pulse">
+                        <StarIcon fill="#E935C1" />
+                        <p>PRO</p>
+
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+
+                    <DropdownMenuItem>Manage</DropdownMenuItem>
+                    
+                    </>
+                )}
                
                 <DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
             </DropdownMenuContent>
