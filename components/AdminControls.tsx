cd5d25dc0bca266e0ae903ba16ed4fcec6f6b0b6
north-tower <muneshmyke@gmail.com>
2024-06-02 15:44:20 +0300

    df

--- components/AdminControls.tsx
+++ components/AdminControls.tsx
@@ -1,9 +1,10 @@
+import InviteUser from "./InviteUser"
 
 
 function AdminControls({ chatId }: { chatId: string}) {
   return (
     <div className="flex justify-end space-x-2 m-5 mb-0">
-
+      <InviteUser chatId={chatId} />
     </div>
   )
 }
--- components/InviteUser.tsx
+++ components/InviteUser.tsx
@@ -17,6 +17,11 @@ import { useSubscriptionStore } from "@/store/store";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { PlusCircleIcon } from "lucide-react";
+import { ChatMembersRef, addChatRef } from "@/lib/converters/ChatMembers";
+import { getDocs, serverTimestamp, setDoc } from "firebase/firestore";
+import { ToastAction } from "./ui/toast";
+import { getUserByEmailRef } from "@/lib/converters/User";
+
 
 
 const formSchema = z.object({
@@ -30,7 +35,7 @@ function InviteUser({ chatId }: { chatId: string}) {
     const subscription = useSubscriptionStore((state) => state.subscription);
     const router = useRouter();
 
-    const [opem , setOpen] = useState(false);
+    const [open , setOpen] = useState(false);
     const [openInviteLink, setOpenInviteLink] = useState(false);
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -39,10 +44,81 @@ function InviteUser({ chatId }: { chatId: string}) {
             email: "",
         },
     });
+
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        if(!session?.user.id) return;
+
+        toast({
+            title: "Sending invite",
+            description: 'Please wait while we send the inivite...'
+        });
+
+        const noOfUsersInChat = (await getDocs(ChatMembersRef(chatId))).docs.map(
+            (doc) => doc.data()
+        ).length;
+
+        const isPro = subscription?.role === "pro" && subscription.status === "active";
+
+        if(!isPro && noOfUsersInChat >= 2) {
+            toast({
+                title: "Free plan limit exceeded",
+                description: "You have exceeded the limit of users in a single chat for the FREE plan.Please upgrade to PRO to continue adding users to chats ",
+                variant: "destructive",
+                action: (
+                    <ToastAction altText="Upgrade"
+                    onClick={() => router.push("/regitser")}>
+                        Upgrade to PRO
+                    </ToastAction>
+                ),
+            });
+            return;
+        }
+        const querySnapshot = await getDocs(getUserByEmailRef(values.email));
+
+        if (querySnapshot.empty){
+            toast({
+                title: "User not found",
+                description: "Please enter an email address of a registered user",
+                variant: "destructive",
+            });
+            return;
+        } else {
+            const user = querySnapshot.docs[0].data();
+
+            await setDoc(addChatRef(chatId, user.id), {
+                userId: user.id!,
+                email: user.email!,
+                timestamp: serverTimestamp(),
+                chatId: chatId,
+                isAdmin: false,
+                image: user.image || "",
+            }).then(() => {
+                setOpen(false);
+
+                toast({
+                    title: "Added to chat",
+                    description: "The user has been added to the chat successfully",
+                    className: "bg-green-600 text-white",
+                    duration: 3000,
+                });
+                setOpenInviteLink(true);
+            }).catch(() => {
+                toast({
+                    title: "Error",
+                    description: "Whoops... there was an error adding the user to the chat!",
+                    variant: "destructive",
+                });
+                setOpen(false);
+        })
+        }
+        form.reset();
+    }
+
   return (
     adminId === session?.user.id && (
         <>
-        <Dialog open={open} asOpenChange={setOpen}>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <Button>
                     <PlusCircleIcon className="mr-1" />
@@ -55,10 +131,32 @@ function InviteUser({ chatId }: { chatId: string}) {
                     <DialogDescription>
                         Simply enter another users email address to invite them to this chat!{" "}
                         <span className="text-indigo-600 font-bold">
-                            (Note: they must be registered)
+                            (Note: they must be registered )
                         </span>
                     </DialogDescription>
                 </DialogHeader>
+                
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)}
+                    className="flex flex-col space-y-2"
+                    >
+                        <FormField control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormControl>
+                                   <Input placeholder=" john@doe.com" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+                        <Button className="ml-auto sm:w-fit w-full" type="submit">
+                            Add To Chat
+                        </Button>
+
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
         
