ffda5274cc1ec421a43af4f9bb290841ffaf4483
north-tower <muneshmyke@gmail.com>
2024-06-02 13:09:34 +0300

    b

--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -5,6 +5,7 @@ import { MessageCircleIcon } from 'lucide-react';
 import { Session } from 'next-auth';
 import React, { createRef, useEffect } from 'react'
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import UserAvatar from './UserAvatar';
 
 function ChatMessages({
     chatId, initialMessages,session
@@ -29,9 +30,9 @@ function ChatMessages({
   return (
     <div>
         {!loading && messages?.length === 0 && (
-            <div className='flex flex-col justify-center  text-center items-center p-20
+        <div className='flex flex-col justify-center  text-center items-center p-20
             rounded-xl space-y-2 bg-indigo-400 text-white font-extralight'>
-                <MessageCircleIcon className='h-10 w-10' />
+            <MessageCircleIcon className='h-10 w-10' />
                 <h2>
                     <span className='font-bold'>Invite a friend</span> & {" "}
                     <span className='font-bold'>
@@ -39,9 +40,32 @@ function ChatMessages({
                     </span>{" "}
                     below to get started!
                 </h2>
-                </div>
-                
+        </div>      
         )}
+
+        {messages?.map((message) => {
+            const isSender = message.user.id === session?.user.id;
+
+            return (
+                <div key={message.id} className='flex my-2 items-end'>
+                    <div className={`flex flex-col relative space-y-2 p-4 w-fit line-clamp-1
+                    mx-2 rounded-lg ${isSender ? "ml-auto bg-violet-600 text-white rounded-br-none" :
+                        "bg-gray-100 dark:text-gray-100 dark:bg-slate-700 rounded-bl-none"
+                    }`}>
+                        <p className={`text-xs italic font-extralight line-clamp-1 ${isSender ? "text-right" : "text-left"}`}>
+                            {message.user.name.split(" ")[0]}
+                        </p>
+                        <div className="flex space-x-2">
+                            <p>{message.input}</p>
+                        </div>                      
+                    </div>
+                    <UserAvatar name={message.user.name}
+                    image={message.user.image}
+                    className={`${!isSender && "-order-1"}`}
+                    />
+                    </div>
+            )
+        })}
     </div>
   )
 }
