8563806944172a87c75f18b13814bd20b8d2d8ae
north-tower <muneshmyke@gmail.com>
2024-06-02 16:20:22 +0300

    z

--- components/ShareLink.tsx
+++ components/ShareLink.tsx
@@ -1,10 +1,34 @@
 import React, { Dispatch, SetStateAction } from 'react'
+import { useToast } from './ui/use-toast';
 
 function ShareLink({
     isOpen, chatId, setIsOpen
 }: {
     isOpen: boolean, chatId: string, setIsOpen: Dispatch<SetStateAction<boolean>>;
 }) {
+ 
+    const { toast } = useToast();
+    const host = window.location.host;
+
+    const linkToChat = 
+    process.env.NODE_ENV === "development" ?
+    `https://${host}/chat/${chatId}` : 
+     `https://${host}/chat/${chatId}`;
+
+     async function copyToClipboard() {
+        try {
+            await navigator.clipboard.writeText(linkToChat);
+            console.log("Tet copied to slipboard");
+
+            toast({
+                title: "Copied successfuly",
+                description: "Share this to the person",
+                className: "bg-green-600 text-white"
+            });
+        } catch(err) {
+            console.error("Faild to copy text:", err);
+        }
+     }
   return (
     <div>ShareLink</div>
   )
