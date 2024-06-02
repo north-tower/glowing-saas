dbc0d0eb3e1fef607cc1919beb9c944cefa7607a
north-tower <muneshmyke@gmail.com>
2024-06-02 08:37:00 +0300

    ui

--- components/ChatList.tsx
+++ components/ChatList.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { authOptions } from '@/auth'
 import { getDocs } from 'firebase/firestore'
 import { getServerSession } from 'next-auth'
+import { chatMembersCollectionGroupRef } from '@/lib/converters/ChatMembers';
 
 async function ChatList() {
     const session = await getServerSession(authOptions);
+
+    const chatsSnapshot = await getDocs(
+      chatMembersCollectionGroupRef(session?.user.id!)
+    )
 
   return (
     <div>ChatList</div>
