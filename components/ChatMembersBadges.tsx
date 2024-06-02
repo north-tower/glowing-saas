1b952f005d68650e5e34956a51160b0f643e9ed4
north-tower <muneshmyke@gmail.com>
2024-06-02 13:43:06 +0300

    t

--- components/ChatMembersBadges.tsx
+++ components/ChatMembersBadges.tsx
@@ -1,8 +1,12 @@
+'use client'
+
 import useAdminId from '@/hooks/useAdminId';
 import { ChatMembers, ChatMembersRef } from '@/lib/converters/ChatMembers'
 import React from 'react'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import LoadingSpinner from './LoadingSpinner';
+import { Badge } from './ui/badge';
+import UserAvatar from './UserAvatar';
 
 function ChatMembersBadges({chatId}: {chatId: string}) {
     const [members, loading, error] = useCollectionData<ChatMembers>(
@@ -12,9 +16,31 @@ function ChatMembersBadges({chatId}: {chatId: string}) {
     const adminId = useAdminId({ chatId });
 
     if (loading && !members) return <LoadingSpinner />;
-    
+
   return (
-    <div>ChatMembersBadges</div>
+    !loading && (
+        <div className='p-2 border rounded-xl m-5'>
+            <div className='flex flex-wrap justify-center md:justify-start items-center
+            gap-2 p-2'>
+                {members?.map((member) => (
+                    <Badge variant="secondary" 
+                    key={member.email}
+                    className='h-14 p-5 pl-2 pr-5 flex space-x-2'>
+                        <div className='flex items-center space-x-2'>
+                            <UserAvatar name={member.email} image={member.image} />
+                        </div>
+
+                        <div>
+                            <p>{member.email}</p>
+                            {member.userId === adminId && (
+                                <p className='text-indigo-400 animate-pulse'>Admin</p>
+                            )}
+                        </div>
+                    </Badge>
+                ))}
+            </div>
+        </div>
+    )
   )
 }
 
